56e5aa070eee915bba6b99eb7ce04d8d332fcec5
Andreas Maier <andreashmaier@gmail.com>
2014-02-13 09:05:37 +0100

    Kommentar in main hinzugefügt

--- GUI_Farbenwahl/src/de/i3a/JMain.java
+++ GUI_Farbenwahl/src/de/i3a/JMain.java
@@ -1,7 +1,12 @@
 package de.i3a;
-
+/**
+ * 
+ * @author Andreas Maier
+ *
+ */
 public class JMain {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Fenster();
